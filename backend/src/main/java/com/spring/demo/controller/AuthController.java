329e87696edff436daf2339f18e7342d398f56cb
Uvindu2002 <wupu0327@gmail.com>
2025-04-21 16:42:59 +0530

    create sign up and login api

---
+++ backend/src/main/java/com/spring/demo/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.spring.demo.controller;
+
+import com.spring.demo.dto.JwtResponse;
+import com.spring.demo.dto.LoginRequest;
+import com.spring.demo.dto.SignupRequest;
+import com.spring.demo.model.User;
+import com.spring.demo.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody SignupRequest request) {
+        return ResponseEntity.ok(authService.signup(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest request) {
+        JwtResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ backend/src/main/java/com/spring/demo/service/AuthService.java
@@ -0,0 +1,55 @@
+package com.spring.demo.service;
+
+import com.spring.demo.dto.LoginRequest;
+import com.spring.demo.dto.SignupRequest;
+import com.spring.demo.dto.JwtResponse;
+import com.spring.demo.exception.CustomException;
+import com.spring.demo.model.User;
+import com.spring.demo.repository.UserRepository;
+import com.spring.demo.util.JwtUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepository, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public User signup(SignupRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new CustomException("Username is already taken!");
+        }
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new CustomException("Email is already in use!");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword()); // plaintext or encoded
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setBio("");
+        user.setProfileImageUrl("");
+
+        return userRepository.save(user);
+    }
+
+    public JwtResponse login(LoginRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new CustomException("User not found"));
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new CustomException("Invalid credentials");
+        }
+
+        String token = jwtUtil.generateToken(user.getUsername());
+        return new JwtResponse(token);
+    }
+}
