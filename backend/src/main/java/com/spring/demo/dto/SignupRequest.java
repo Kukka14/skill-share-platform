bfe4ca21e28a6386e4d2763c8b5bb96d55644510
Nethkavee7970 <hettithanthrigenethmi2000@gmail.com>
2025-04-22 12:12:34 +0530

    add notification when sign up

--- backend/src/main/java/com/spring/demo/dto/SignupRequest.java
+++ backend/src/main/java/com/spring/demo/dto/SignupRequest.java
@@ -1,12 +1,55 @@
 package com.spring.demo.dto;
 
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 public class SignupRequest {
     private String username;
     private String email;
     private String password;
     private String firstName;
     private String lastName;
+
+//added by nethmi.......
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
