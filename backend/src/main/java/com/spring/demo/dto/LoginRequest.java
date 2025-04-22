fd157349a3e1ec5e029be716380fe30e4bd3eced
Nethkavee7970 <hettithanthrigenethmi2000@gmail.com>
2025-04-22 13:06:10 +0530

    comment getters and setters

--- backend/src/main/java/com/spring/demo/dto/LoginRequest.java
+++ backend/src/main/java/com/spring/demo/dto/LoginRequest.java
@@ -1,8 +1,8 @@
 package com.spring.demo.dto;
 
-//import lombok.Data;
+import lombok.Data;
 
-//@Data
+@Data
 public class LoginRequest {
     private String username;
     private String password;
@@ -10,20 +10,22 @@ public class LoginRequest {
 
     
     // Getter and Setter for username--->>added by nethmi
-    public String getUsername() {
-        return username;
-    }
+    
+    // public String getUsername() {
+    //     return username;
+    // }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    // public void setUsername(String username) {
+    //     this.username = username;
+    // }
 
     // Getter and Setter for password
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // public String getPassword() {
+    //     return password;
+    // }
+
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
 }
--- backend/src/main/java/com/spring/demo/dto/SignupRequest.java
+++ backend/src/main/java/com/spring/demo/dto/SignupRequest.java
@@ -1,8 +1,8 @@
 package com.spring.demo.dto;
 
-//import lombok.Data;
+import lombok.Data;
 
-//@Data
+@Data
 public class SignupRequest {
     private String username;
     private String email;
@@ -12,44 +12,44 @@ public class SignupRequest {
 
 //added by nethmi.......
     // Getters
-    public String getUsername() {
-        return username;
-    }
+    // public String getUsername() {
+    //     return username;
+    // }
 
-    public String getEmail() {
-        return email;
-    }
+    // public String getEmail() {
+    //     return email;
+    // }
 
-    public String getPassword() {
-        return password;
-    }
+    // public String getPassword() {
+    //     return password;
+    // }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    // public String getFirstName() {
+    //     return firstName;
+    // }
 
-    public String getLastName() {
-        return lastName;
-    }
+    // public String getLastName() {
+    //     return lastName;
+    // }
 
     // // Setters
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    // public void setUsername(String username) {
+    //     this.username = username;
+    // }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    // public void setFirstName(String firstName) {
+    //     this.firstName = firstName;
+    // }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    // public void setLastName(String lastName) {
+    //     this.lastName = lastName;
+    // }
 }
