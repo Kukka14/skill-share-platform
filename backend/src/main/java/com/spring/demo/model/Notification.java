758d18dfc4174f87636f60ee4ccd3180e8035833
Nethkavee7970 <hettithanthrigenethmi2000@example.com>
2025-04-24 22:58:19 +0530

    update crud to like

--- backend/src/main/java/com/spring/demo/model/Notification.java
+++ backend/src/main/java/com/spring/demo/model/Notification.java
@@ -16,8 +16,76 @@ public class Notification {
     private String notificationId;
     private String userId;
     private String username;
+    private String postId;
+    private String postUserId;
     private String description;
     private boolean isRead;
     private LocalDateTime timestamp;
+
+
+    public String getNotificationId() {
+        return notificationId;
+    }
+    
+    public void setNotificationId(String notificationId) {
+        this.notificationId = notificationId;
+    }
+    
+    public String getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPostId() {
+        return postId;
+    }
+    
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+    
+    public String getPostUserId() {
+        return postUserId;
+    }
+    
+    public void setPostUserId(String postUserId) {
+        this.postUserId = postUserId;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public boolean isRead() {
+        return isRead;
+    }
+    
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+    
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    
 
 }
